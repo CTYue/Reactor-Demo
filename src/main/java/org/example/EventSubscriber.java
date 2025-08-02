c3e5471dc90eabbb7151676c1a6d03513098ace9
CAnonymous <zidong.yu@outlook.com>
2025-08-01 21:57:32 -0700

    Add comment.

--- src/main/java/org/example/EventSubscriber.java
+++ src/main/java/org/example/EventSubscriber.java
@@ -4,6 +4,7 @@ import reactor.core.publisher.Flux;
 
 public class EventSubscriber {
     public void subscribeToEvents(Flux<String> events) {
+        //Events will be executed only when subscribed (lazy execution)
         events.subscribe(event -> System.out.println("Event subscriber receives: " + event + " on Thread: "+
                 Thread.currentThread().getName()),
                 error -> System.err.println("Event subscriber error: "+error),
