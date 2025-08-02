8090f395aaf1bd1e65cde77849e06cf6cc2c22ce
CAnonymous <zidong.yu@outlook.com>
2025-08-01 23:01:19 -0700

    add timestamp to show that each subscription executes publisher on its own.

--- src/main/java/org/example/EventPublisher.java
+++ src/main/java/org/example/EventPublisher.java
@@ -5,11 +5,12 @@ import reactor.core.publisher.Flux;
 import java.time.Duration;
 
 public class EventPublisher {
+
     public Flux<String> publishEvents() {
         return Flux.just("Event 4", "Event 5", "Event 6")
                 .delayElements(Duration.ofSeconds(2))
-                .doOnSubscribe(subscription -> System.out.println("Event publisher subscribed"))
+                .doOnSubscribe(subscription -> System.out.println("Event publisher subscribed at "+System.currentTimeMillis()))
                 .doOnNext(event -> System.out.println("Event publisher emits: " + event))
-                .doOnComplete(() -> System.out.println("Event publisher completed"));
+                .doOnComplete(() -> System.out.println("Event publisher completed at "+System.currentTimeMillis()));
     }
 }
